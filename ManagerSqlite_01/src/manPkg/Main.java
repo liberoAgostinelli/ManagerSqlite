bf40a59b4bec6f3727ac0831d12677534cc19c93
molly <molly@molly-Modern-15-A11M>
2023-02-21 20:50:50 +0100

    Primo commit dell'app, implementato create database

---
+++ ManagerSqlite_01/src/manPkg/Main.java
@@ -0,0 +1,14 @@
+package manPkg;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Model model = new Model();
+		View view = new View();
+		Controller controller = new Controller(view, model);
+		
+	}
+
+}
